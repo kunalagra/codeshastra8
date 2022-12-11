85c840adc357d45fd9f2242395e40a69a57bd9ee
kunalrag <goku.agra@yahoo.in>
2022-12-11 19:26:18 +0530

    Provide Secret keys for session

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 from views import *
 
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
 if __name__ == "__main__":
-    app.secret_key = 'super secret key'
-    app.config['SESSION_TYPE'] = 'filesystem'
     app.run(debug="true")
