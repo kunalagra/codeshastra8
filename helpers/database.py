fd0d52b3e4b526168858cd465f83510ca91e34bc
kunalrag <goku.agra@yahoo.in>
2022-12-11 19:09:43 +0530

    Remove Revoked Creds

--- helpers/database.py
+++ helpers/database.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify, request, abort, render_template
 app = Flask(__name__)
 
 # Google Sheets API Setup
-credential = ServiceAccountCredentials.from_json_keyfile_name("{}\\helpers\\cred.json".format(os.getcwd()),["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets",
+credential = ServiceAccountCredentials.from_json_keyfile_name("/etc/secrets/cred.json",["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets",
 "https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"])
 
 client = gspread.authorize(credential)
